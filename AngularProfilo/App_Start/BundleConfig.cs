1c6a0c20cad782d9d316c880e9ece9ce712c906b
Patrick Yee <g0th1cazn@gmail.com>
2018-05-09 15:50:14 -0600

    Learning how to integrate Angular with ASP.NET MVC

--- AngularProfilo/App_Start/BundleConfig.cs
+++ AngularProfilo/App_Start/BundleConfig.cs
@@ -25,6 +25,13 @@ namespace AngularProfilo
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/Angular").Include(
+                "~/main.js",
+                "~/polyfills.js",
+                "~/runtime.js",
+                "~/styles.js",
+                "~/vendor.js"));
         }
     }
 }
